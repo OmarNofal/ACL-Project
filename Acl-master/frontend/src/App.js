2f7effd8c6ab2c08d8cdfe5fed8529924c455b84
Omar Walid <omarwalidhamed@gmail.com>
2022-11-05 20:06:28 +0100

    Setup React Frontend

---
+++ Acl-master/frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Course Project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
