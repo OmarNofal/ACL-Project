3c463012e0df03223c2441c7c22f681ca72fcf5a
zeyad <zeyadehab20@yahoo.com>
2022-12-13 19:23:36 +0100

    lllll

--- Acl-master/frontend/src/components/SelectCountry.jsx
+++ Acl-master/frontend/src/components/SelectCountry.jsx
@@ -4,23 +4,32 @@ import axios from 'axios'
 
 function SelectCountry() {
     const [data,setData]=useState([])
-    const[getCountry,setCountry]=useState()
+    const[countryid,setCountryid]=useState()
+
     useEffect(()=>{
         axios.get('https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json')
         .then(res=>setData(res.data))
         .catch(err=>console.log(err))
 
     },[])
+console.log("")
+    const handleCountry=(event)=>{
+        const getcountryid=event.target.value
+        //console.log(getcountryid)
+        setCountryid(getcountryid)
+        localStorage.setItem('country',JSON.stringify(getcountryid))
+
+    }
 
 const country=[...new Set(data.map(item=>item.country))]
-    
+   //console.log(country) 
   return (
     <div >
         <div>
             <label>Country</label>
-            <select>
+            <select onChange={(e)=>handleCountry(e)}>
                 <option value="">Select Country</option>
-                {country.map(items=><option key={items}>{items}</option>)}
+                {country.map(items=><option value={items} key={items}>{items}</option>)}
             </select>
         </div>
     </div>
