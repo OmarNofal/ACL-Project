38cb2ed92e9467f485e2be45f3f2313cea78e749
mamamiaato <116451886+mamamiaato@users.noreply.github.com>
2023-01-18 17:31:39 +0100

    212121

--- Acl-master/frontend/src/pages/Course.jsx
+++ Acl-master/frontend/src/pages/Course.jsx
@@ -1,8 +1,9 @@
-
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 //import { makeStyles } from '@mui/material/core/styles';
+import { useHistory } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-
+import axios from "axios";
 import Rating from '@mui/material/Rating';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -15,6 +16,10 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 
 function CoursePage() {
+    const user={
+        "Username":"trainee4",
+        "Password": "$2a$10$3SI1dkp3l4NTKI0Z7kqhU.ibpmNSKsvsN6h0N7zrYVUCk2Ac63oce"
+    };
     const theme = useTheme();
     const [instructorRating, setInstructorRating] = useState(0);
     const [open, setOpen] = useState(false);
@@ -26,6 +31,7 @@ function CoursePage() {
 
   
     const handleWatchVideos=()=>{
+        setProgress(progress + 10)  
         
     }
 
@@ -51,16 +57,62 @@ setOpenReport(false);
 const handleRefund = () => {
 if (progress < 50) {
 // make a refund request to the backend
+axios.post('http://localhost:8000/api/users/requestRefundTrainee', {
+    username: 'Trainee4',
+    courseTitle: 'csen101'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  handleClose(true);
 } else {
 alert('Cannot request a refund as progress is over 50%');
 }
 };
 
 const handleReport = (text) => {
+
 // make a request to the backend with the text as the problem description
-console.log(text);
+axios.post('http://localhost:8000/api/users/reportProblem', {
+    username: 'Trainee4',
+    title: 'csen101',
+    type: text
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+//console.log(text);
 setOpenReport(false);
 };
+
+
+const handleDownloadCertificate=()=>{
+    axios.get('http://localhost:8000/api/users/downloadCertificate')
+  .then(function (response) {
+    console.log(response);
+  })
+}
+
+
+const handleRecieveCertificate=()=>{
+    axios.get('http://localhost:8000/api/users/recieveCertificate')
+  .then(function (response) {
+    console.log(response);
+  })
+}
+
+
+const handleProgress=()=>{
+    axios.get('http://localhost:8000/api/users/viewProgressInCourse', {
+        Username:"trainee4",
+        courseTitle:"csen101"
+    })
+    .then(function (response) {
+      console.log(response);
+      setProgress(response);
+    })
+
+}
 
 return (
 <div>
@@ -69,9 +121,11 @@ return (
 Progress: {progress}%
 </div>
 <div style={{display: "flex", justifyContent: "space-around", marginTop: "30px"}}>
-<Button variant="contained" onClick={() => setProgress(progress + 10)}>
-Solve a Quiz
-</Button>
+<Link to="/Quiz">
+            <Button variant="contained" onClick={() => setProgress(progress + 10)}>
+                Solve a Quiz
+            </Button>
+        </Link>
 <Button variant="contained" onClick={handleWatchVideos}>
 Watch Course Videos
 </Button>
@@ -87,8 +141,12 @@ Report a Problem
 </div>
 {progress === 100 ? (
 <div style={{display: "flex", justifyContent: "center", marginTop: "30px"}}>
-<Button variant="contained" onClick={() => downloadCertificate()}>
+<Button variant="contained" onClick={() => handleDownloadCertificate()}>
 Download Certificate
+</Button>
+<br></br>
+<Button variant="contained" onClick={() => handleRecieveCertificate()}>
+Recieve Certificate Via Mail
 </Button>
 </div>
 ) : null}
@@ -140,6 +198,18 @@ Confirm
     Rate The Course:
 <Rating name="course-rating" value={rating} onChange={(event, newValue) => {setRating(newValue);
 // send the rating to the backend
+axios.post('http://localhost:8000/api/users/rateCourse', {
+    user,
+    titleCourse:"csen101",
+    rating:rating 
+  }, {
+    headers: {
+        'Content-Type': 'application/json'
+    }})
+  .then(function (response) {
+    console.log(response);
+  })
+
 }}
 />
 </div>
